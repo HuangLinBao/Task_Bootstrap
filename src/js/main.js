2b4eb4afe03e0870d8cf8022084f2de95373764a
HuangLinBao <tariq.92@live.com>
2023-11-27 14:40:07 +0200

    done

---
+++ src/js/main.js
@@ -0,0 +1,73 @@
+import "../scss/styles.scss";
+import * as bootstrap from "bootstrap";
+
+document.querySelector("#app").innerHTML = `
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active c-item">
+      <img src="https://news.iu.edu/live/image/gid/2/width/1000/height/893/8840_3dbd719a-2a67-436c-8664-24d18f599a72.rev.1690208832.jpg" class="d-block w-100 c-img" alt="...">
+    </div>
+    <div class="carousel-item c-item">
+      <img src="https://t4.ftcdn.net/jpg/05/39/11/95/360_F_539119529_ZlgsE2EZQtS0YBAlXnKUkiNkb8PWgy9z.jpg" class="d-block w-100 c-img" alt="...">
+    </div>
+    <div class="carousel-item c-item">
+      <img src="https://static.vecteezy.com/system/resources/previews/030/763/215/large_2x/portrait-of-a-elephant-wearing-glasses-and-a-suit-copy-space-generative-ai-free-photo.jpg" class="d-block w-100 c-img" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div class="container text-center m-5">
+    <div class="row">
+        <div class="col-12">
+            86 d8 a7 20 d8 b2 d8 a7 d8 b1 d9 86 d8 a7 20 d9 81 d9 8a d9 84 20 d8 b8 d8 b1 d9 8a d9 81 0a 0a d8 a8 d8 b1 d9 81 d9 82 20 d9 82 d8 a7 d9 84 20 d9 84 d9 86 d8 a7 20 d9 84 
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            d9 8a d8 b3 20 d9 87 d9 86 d8 a7 d9 84 d9 83 20 d9 85 d8 a7 20 d9 8a d8 ae d9 8a d9 81 0a 0a d9 86 d8 ad d9 86 20 d8 a7 d9 84 d8 ae d9 8a d8 b1 20 d8 a8 d8 b7 d8 a8 d8 b9 
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            d9 86 d8 a7 20 d9 84 d8 a7 20 d9 86 d8 b1 d8 b6 d9 89
+        </div>
+        <div class="col-4">
+            20 d8 b8 d9 84 d9 85 20 d8 a7 d9 84 d8 b6 d8 b9 d9 8a
+        </div>
+        <div class="col-4">
+            d9 81 0a 0a d9 84 d8 a7 20 d9 8a d8 ad d9 8a d8 a7 20
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            d8 a8 d9 8a d9 86 d9 86 d8 a7 20 d8 a7 d9 84 d8 a7 20
+        </div>
+        <div class="col-4">
+            d8 a7 d9 84 d8 a7 d9 86 d8 b3 d8 a7 d9 86 20 d8 a7 d9
+        </div>
+        <div class="col-4">
+            84 d8 b4 d8 b1 d9 8a d9 81 0a 0a d9 85 d8 b1 d8 a9 20
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+        d9 81 d9 8a 20 d8 ad d9 8a d9 86 d8 a7 20 d8 b2 d8 a7 d8 b1 d9 86 d8 a7 20 d9 81 d9 8a d9 84 20 d8 b8 d8 b1 d9 8a
+        </div>
+        <div class="col-4">
+        d9 81 0a 0a d8 a8 d8 b1 d9 81 d9 82 20 d9 82 d8 a7 d9
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+        84 20 d9 84 d9 86 d8 a7 20 d9 84 d9 8a d8 b3 20 d9 87 d9 86 d8 a7 d9 84 d9 83 20 d9 85 d8 a7 20 d9 8a d8 ae d9 8a d9 81 0a 0a d9 86 d8 ad d9 86 20 d8 a7 d9 84 d8 ae d9 8a 
+        </div>
+    </div>
+</div>
+`;
